1ffb42c8aa4d8198a79c61300fcb2c039e8da585
almenal <almenal99@gmail.com>
2020-04-15 21:56:38 +0200

    Rutinary raw data update

--- scripts/plots-covid-timeseries.r
+++ scripts/plots-covid-timeseries.r
@@ -50,7 +50,7 @@ p1 = ggplot(df_sub, aes(timenorm_num, conf, col = loc)) +
        title = 'Number of confirmed cases by country',
        col = 'Country')
 
-p1
+#p1
 
 ggsave(plot = p1, width = 1920/320, height = 1080/320, units = 'in', scale = 1.75,
        filename = paste0('../plots/conf_cases_after_', thresh_pat, 'th_patient.svg'))
@@ -68,12 +68,12 @@ p2 = ggplot(df_sub, aes(timenorm_num, y = growth_con_5, col = loc)) +
        y = 'Growth rate respective to previous 5 days',
        title = 'Growth rate of cases by country',
        col = 'Country')
-p2
-ggsave(plot = p1, filename = '../plots/growth-rates.png',
+#p2
+ggsave(plot = p2, filename = '../plots/growth-rates.png',
        dpi = 300, width = 1920/300, height = 1080/300, units = 'in', scale = 1.75)
 
-p2p = plotly::ggplotly(p2)
 pdf(NULL) # Prevent the automatic generation of "Rplots.pdf"
+#p2p = plotly::ggplotly(p2)
 #htmlwidgets::saveWidget(plotly::as_widget(p2p), "../plots/growth-rate.html")
 
 ### Detection of kth patient per country -----
@@ -89,12 +89,12 @@ p3 = ggplot(days_sub, aes(x = loc, y = day0)) +
   labs(y = 'Day of detection of 100th patient', x = 'Country') +
   coord_flip()
 
-p3
+#p3
 
 ### Active cases -------------------
 p4 = ggplot(df_sub, aes(x = timenorm_num, y = active, col = loc)) + 
   geom_point() + geom_line()
-p4
+#p4
 
 #F8766D
 cols = c('Active' = '#E08B00', 'Recovered-cumul' = '#7CAE00', 'Died-cumul' = 'red', 'Total-cumul' = 'darkgrey')
@@ -106,7 +106,7 @@ p5 = ggplot(df_sub, aes(x = timenorm_num)) +
   scale_fill_manual(name="Legend",values=cols) + 
   theme(legend.position = 'top')+
   facet_wrap(~loc)
-p5
+#p5
 
 ggsave('../plots/epidemic-curves.svg', plot=p5, scale = 1.65)
 ggsave('../plots/epidemic-curves.png', plot=p5, scale = 1.65)
