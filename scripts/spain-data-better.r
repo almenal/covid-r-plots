2bf9950f77550bb9179090d80298fb1fee43a72e
almenal <almenal99@gmail.com>
2020-08-07 12:01:21 +0200

    Rutinary raw data update

--- scripts/spain-data-better.r
+++ scripts/spain-data-better.r
@@ -1,13 +1,19 @@
-sapply(c('dplyr', 'ggplot2', 'tidyr'), require, character.only = T)
+sapply(c('dplyr', 'ggplot2', 'tidyr', 'plotly'), require, character.only = T)
+# ccaa <- readr::read_csv('https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_casos_long.csv') # Outdated
 ccaa <- readr::read_csv('https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_datos_isciii_nueva_serie.csv')
 ccaa <- ccaa %>%
-  arrange(ccaa, fecha) %>%
-  mutate(nuevos = c(NA, diff(num_casos)),
-         nuevos = ifelse(fecha == min(fecha), NA, num_casos))
-
+  arrange(ccaa, fecha) #%>%
+  # mutate(nuevos = c(NA, diff(num_casos)),
+  #        nuevos = ifelse(fecha == min(fecha), NA, num_casos))
+  # num_casos muestra los casos nuevos
 
 ggplot(ccaa, aes(fecha, num_casos, color = ccaa)) + geom_line()
-ggplot(ccaa, aes(fecha, nuevos, color = ccaa)) + geom_line()
+
+ggplot(ccaa %>% filter(ccaa == 'Andalucía'),
+       aes(fecha, num_casos, color = ccaa)) + 
+  geom_line()
+ggplotly()
+
 
 lastdate = max(ccaa$fecha)
 ccaa %>%
